647bd7b557e4ab6a554f3e727257f426b7b1ba2b
Matthias Loker <mmloker@gmail.com>
2015-08-08 16:51:30 +0200

    Add TodoItem model and initial styling for index

---
+++ app/models/TodoItem.rb
@@ -0,0 +1,32 @@
+class TodoItem
+  def initialize(description)
+    @description = description
+    @finished = false
+  end
+
+  def description
+    @description
+  end
+
+  def description=(description)
+    @description = description
+  end
+
+  def finished
+    @finished
+  end
+
+  def finished=(boolean)
+    @finished = boolean
+  end
+
+  def self.all
+    desc_one = "Go running"
+    desc_two = "Buy groceries"
+    desc_three = "Study Chinese"
+    todos = [desc_one, desc_two, desc_three]
+    todos.map do |todo|
+      self.new(todo)
+    end
+  end
+end
